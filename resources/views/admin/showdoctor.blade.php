a17d36191665d84cd6da9ce62cad80586af2ef7a
nokibulridoy <nokibulislam720@gmail.com>
2023-01-02 16:51:42 +0600

    sss

--- resources/views/admin/showdoctor.blade.php
+++ resources/views/admin/showdoctor.blade.php
@@ -28,18 +28,33 @@
         			<tr style="background-color:black;">
         				<th style="padding:10px">Doctor Name</th>
     					<th style="padding:10px">Phone</th>
-    					<th style="padding:10px">Specility</th>
+    					<th style="padding:10px">Speciality</th>
     					<th style="padding:10px">Room No</th>
     					<th style="padding:10px">Image</th>
+              <th style="padding:10px">Delete</th>
+              <th style="padding:10px">Update</th>
     				</tr>
 
     				@foreach($data as $doctor)
 
-    				<tr>
+    				<tr align="center" style="background-color:skyblue;">
     					<td>{{$doctor->name}}</td>
     					<td>{{$doctor->phone}}</td>
-    					<td>{{$doctor->specility}}</td>
+    					<td>{{$doctor->speciality}}</td>
     					<td>{{$doctor->room}}</td>
+              <td><img height="100" width="100" src="doctorimage/{{$doctor->image}}">
+              </td>
+
+              <td><a onclick="return confirm('are you sure delete this')" class="btn btn-danger" href="{{url('deletedoctor',$doctor->id)}}">Delete</a>
+              </td>
+
+              <td><a class="btn btn-primary" href="{{url('updatedoctor',$doctor->id)}}">Update</a>
+              </td>
+
+
+
+
+              
     					
     				</tr>
 
